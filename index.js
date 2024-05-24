248d4fd744e9e35e7c350d2b853150e9a076285d
Jony <96882@sistemas.frc.utn.edu.ar>
2024-05-24 16:52:44 -0300

    ejercicio completo

--- index.js
+++ index.js
@@ -2,15 +2,40 @@ const express = require("express");
 
 // crear servidor
 const app = express();
+require("./base-orm/sqlite-init");  // crear base si no existe
+app.use(express.json()); // para poder leer json en el body
 
 // controlar ruta
 app.get("/", (req, res) => {
-  res.send("Backend inicial dds-backend!");
+  res.send("Hola mundo!");
 });
 
+const articulosfamiliasRouter = require("./routes/articulosfamilias");
+app.use(articulosfamiliasRouter);
+
+const articulosRouter = require("./routes/articulos");
+app.use(articulosRouter);
+
+// configurar servidor
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*", // origin: 'https://dds-frontend.azurewebsites.net'
+  })
+);
+
+const seguridadRouter = require("./routes/seguridad");
+app.use(seguridadRouter);
+
 // levantar servidor
-const port = 3000;
-app.listen(port, () => {
+if (!module.parent) {   // si no es llamado por otro módulo, es decir, si es el módulo principal -> levantamos el servidor
+  const port = process.env.PORT || 3000;   // en producción se usa el puerto de la variable de entorno PORT
+  app.locals.fechaInicio = new Date();
+  app.listen(port, () => {
     console.log(`sitio escuchando en el puerto ${port}`);
-});
+  });
+}
+module.exports = app; // para testing
+
+
 
